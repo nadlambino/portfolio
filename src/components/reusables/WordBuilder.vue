6658e78b878241e0ad7ae1ab78addb3865af25ec
Nad Lambino <ronaldlambino0000@gmail.com>
2022-05-15 15:43:28 +0800

    Add animations and styles

---
+++ src/components/reusables/WordBuilder.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="word">
+        <span v-for="(letter, key) in word" :key="key">
+            {{ letter === ' ' ? '&nbsp;' : letter }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WordBuilder',
+    props: ['word']
+}
+</script>
+
+<style lang="scss" scoped>
+.word {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
