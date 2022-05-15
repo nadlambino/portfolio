9ebce71355f480dc1291937b11b7453ac0dab83b
Nad Lambino <ronaldlambino0000@gmail.com>
2022-05-15 14:26:00 +0800

    Add new text color and animation

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
 	<section class="home section-active" id="home">
-		<div class="parallax d-flex align-items-center justify-content-center">
-			<div class="welcome p-5">
-				<p class="display-1">Hi, I am <strong>Ronald</strong></p>
-				<p class="display-5">Web Developer</p>
+		<div class="welcome-container row d-flex justify-content-center align-content-center">
+			<div class="image col-sm-12 col-md-6"></div>
+			<div class="welcome col-sm-12 col-md-6">
+				<p class="intro">Hi, I am</p>
+				<p class="name"><strong>Nad</strong></p>
+				<p class="job-title">Web Developer</p>
 			</div>
 		</div>
 	</section>
