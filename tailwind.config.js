f5f921b573583194f84c3669f9455749344b4655
Nad Lambino <ronaldlambino0000@gmail.com>
2022-06-05 19:54:11 +0800

    Add xs screen size in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     join(__dirname, 'src/**/*.{js,ts,tsx,vue}'),
   ],
   theme: {
+    screens: {
+      'xs': '300px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         'green-neon': '#00ff8d',
