6e8b04d50412115136467f015da5ebe568c9f99e
Nad Lambino <ronaldlambino0000@gmail.com>
2022-06-08 19:45:44 +0800

    Add custom animation in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,49 @@
 const { join } = require('path');
 
 module.exports = {
-  content: [
-    "./index.html",
-    join(__dirname, 'src/**/*.{js,ts,tsx,vue}'),
-  ],
-  theme: {
-    screens: {
-      'xs': '300px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-    extend: {
-      colors: {
-        'green-neon': '#00ff8d',
-        'dark-100': '#3a3845',
-        'dark-200': '#32303c'
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./index.html', join(__dirname, 'src/**/*.{js,ts,tsx,vue}')],
+	theme: {
+		screens: {
+			xs: '300px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
+		extend: {
+			colors: {
+				'green-neon': '#00ff8d',
+				'dark-100': '#3a3845',
+				'dark-200': '#32303c',
+			},
+			animation: {
+				'slide-down': 'slide-down 900ms ease-in-out',
+				'slide-up': 'slide-up 900ms ease-in-out',
+			},
+			keyframes: {
+				'slide-down': {
+					'0%': {
+						transform: 'translateY(-100%)',
+						opacity: '0',
+					},
+					'100%': {
+						transform: 'translateY(0)',
+						opacity: '1',
+					},
+				},
+				'slide-up': {
+					'0%': {
+						transform: 'translateY(100%)',
+						opacity: '0',
+					},
+					'100%': {
+						transform: 'translateY(0)',
+						opacity: '1',
+					},
+				},
+			},
+		},
+	},
+	plugins: [],
+};
