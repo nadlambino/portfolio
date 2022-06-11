5a38cdc8ee70172651b8391017776d7871dc8e6b
Nad Lambino <ronaldlambino0000@gmail.com>
2022-06-11 11:49:47 +0800

    Fix navbar styles and behavior

--- src/pages/NavigationBar.vue
+++ src/pages/NavigationBar.vue
@@ -1,36 +1,66 @@
 <template>
-	<nav class="sm:flex justify-between">
+	<nav class="md:flex justify-between">
 		<div class="nav-brand" ref="nav-brand">
 			<span class="font-bold">
 				<router-link to="/">Nad Lambino</router-link>
 			</span>
 			<span class="nav-toggler" @click="handleToggleMenu">
-				<v-icon scale="1.5" name="gi-hamburger-menu" class="open-menu -mr-7" ref="open-menu"></v-icon>
-				<v-icon scale="1.5" name="io-close-sharp" class="close-menu opacity-0" ref="close-menu"></v-icon>
+				<v-icon
+					scale="1.5"
+					name="gi-hamburger-menu"
+					class="open-menu -mr-7"
+					ref="open-menu"
+				></v-icon>
+				<v-icon
+					scale="1.5"
+					name="io-close-sharp"
+					class="close-menu opacity-0"
+					ref="close-menu"
+				></v-icon>
 			</span>
 		</div>
-		<ul class="nav-link -mt-96 opacity-0 sm:mt-auto sm:opacity-100" ref="menu">
-			<router-link to="/"><li>Home</li></router-link>
-			<router-link to="/about"><li>About</li></router-link>
-			<router-link to="/portfolio"><li>Portfolio</li></router-link>
-			<router-link to="/contact"><li>Contact</li></router-link>
+		<ul
+			class="nav-link -mt-96 opacity-0 md:mt-auto md:opacity-100"
+			ref="menu"
+		>
+			<router-link to="/" @click="handleCloseMenu">
+				<li>Home</li>
+			</router-link>
+			<router-link to="/about" @click="handleCloseMenu">
+				<li>About</li>
+			</router-link>
+			<router-link to="/portfolio" @click="handleCloseMenu">
+				<li>Portfolio</li>
+			</router-link>
+			<router-link to="/contact" @click="handleCloseMenu">
+				<li>Contact</li>
+			</router-link>
 		</ul>
 	</nav>
 </template>
 
 <script>
-
 export default {
 	name: 'NavigationBar',
 	data() {
 		return {
-			show: false
-		}
+			show: false,
+		};
+	},
+	watch: {
+		show() {
+			this.handleToggleClass();
+		},
 	},
 	methods: {
 		handleToggleMenu() {
 			this.show = !this.show;
-			const navbrand = this.$refs['nav-brand']
+		},
+		handleCloseMenu() {
+			this.show = false;
+		},
+		handleToggleClass() {
+			const navbrand = this.$refs['nav-brand'];
 			const menu = this.$refs['menu'];
 			// Add $el to access the element inside of icon component
 			const open = this.$refs['open-menu'].$el;
@@ -52,7 +82,7 @@ export default {
 				open.classList.remove('rotate-180');
 				open.classList.remove('opacity-0');
 			}
-		}
-	}
+		},
+	},
 };
 </script>
