dd030360a85a9e624ca8c2346b608f5fa8042007
Nad Lambino <ronaldlambino0000@gmail.com>
2022-06-11 13:14:24 +0800

    update about timeline

--- src/components/reusables/history-timeline/TimelineContent.vue
+++ src/components/reusables/history-timeline/TimelineContent.vue
@@ -1,32 +1,37 @@
 <template>
-    <div :class="['timeline-content', leftPositionClass]">
-        <span class="timeline-dot"></span>
-        <span class="timeline-arrow"></span>
-        <div class="timeline-event">
-            <span class="timeline-date">{{data.date}}</span>
-            <h1 class="timeline-title">{{data.title}}</h1>
-            <p class="timeline-details" v-html="data.details"></p>
-        </div>
-    </div>
+	<div :class="['timeline-content', leftPositionClass]">
+		<span class="timeline-dot"></span>
+		<span class="timeline-arrow"></span>
+		<div class="timeline-event">
+			<span class="timeline-date">{{ dateRange }}</span>
+			<h1 class="timeline-title">{{ data.title }}</h1>
+			<p class="timeline-details" v-html="data.details"></p>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-    name: 'TimelineContent',
-    props: {
-        data: {
-            type: Object,
-            required: true
-        },
-        left: {
-            type: Boolean,
-            default: () => (false)
-        }
-    },
-    computed: {
-        leftPositionClass() {
-            return this.left === true ? 'timeline-content-left' : ''
-        }
-    }
-}
+	name: 'TimelineContent',
+	props: {
+		data: {
+			type: Object,
+			required: true,
+		},
+		left: {
+			type: Boolean,
+			default: () => false,
+		},
+	},
+	computed: {
+		dateRange() {
+			return this.data.hasOwnProperty('until')
+				? `${this.data.date} - ${this.data.until}`
+				: this.data.date;
+		},
+		leftPositionClass() {
+			return this.left === true ? 'timeline-content-left' : '';
+		},
+	},
+};
 </script>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -8,48 +8,58 @@
 import HistoryTimeline from '../components/reusables/HistoryTimeline.vue';
 
 export default {
-    name: "AboutPage",
-    components: { HistoryTimeline },
-    data() {
-        return {
-            histories: [
-                {
-                    date: dayjs('April 2019').format('MMMM YYYY'),
-                    title: 'Graduation Day!',
-                    details: 'I graduated in <strong>Bachelor of Science in Information Technology</strong> from Quezon City University '
-                },
-                {
-                    date: dayjs('May 20 2019').format('DD MMMM YYYY'),
-                    title: 'Journey Started',
-                    details: 'I immediately got hired by Cafe24 Philippines Inc. I learned a lot of things here about web development.'
-                },
-                {
-                    date: dayjs('Aug 2019').format('MMMM YYYY'),
-                    title: 'Worked On A Real Project!',
-                    details: 'This is my first ever experience to work on a real project after my training. I was introduced to Laravel Framework. Around this time, I also learned about React JS.'
-                },
-                {
-                    date: dayjs('Feb 2020').format('MMMM YYYY'),
-                    title: 'New Project',
-                    details: 'I was introduced to my second project. This time is to revamp the project and use Vue JS framework. I immediately got hooked on how easy and beginner friendly Vue JS is.'
-                },
-                {
-                    date: dayjs('May 2020').format('MMMM YYYY'),
-                    title: 'Started A New Project From Scratch',
-                    details: 'This time, I was assigned to a team who will revamp a legacy application and build it from scratch. We used Vue JS here and also Bootstrap.'
-                },
-                {
-                    date: dayjs('May 2021').format('MMMM YYYY'),
-                    title: 'Explore Other Web Technologies',
-                    details: 'I got interested to learn GraphQL, so I did learned it even when I wasn\'t able to apply it on a real project.'
-                },
-                {
-                    date: dayjs('May 2022').format('MMMM YYYY'),
-                    title: 'Explore Other Web Technologies Part 2',
-                    details: 'I started to learn more things aside from everything I use at work. I created small projects with React JS as a refresher. I created simple chat application with React and Firebase. I started to develop my Portfolio and learned TailwindCSS to use it.'
-                }
-            ]
-        }
-    }
+	name: 'AboutPage',
+	components: { HistoryTimeline },
+	data() {
+		return {
+			histories: [
+				{
+					date: dayjs('April 2019').format('MMMM YYYY'),
+					title: 'Graduation Day',
+					details:
+						'I graduated in Quezon City University with the degree in Bachelor of Science in Information Technology',
+				},
+				{
+					date: dayjs('May 20 2019').format('DD MMMM YYYY'),
+					title: 'Career Journey',
+					details:
+						'I got hired by Cafe24 Philippines and worked as a Junior Web Developer.',
+				},
+				{
+					date: dayjs('Aug 2019').format('MMMM YYYY'),
+					title: 'Working On A Real Project',
+					details:
+						'<li>After months of training, I was deployed on my first project to support in the development and maintain the application.</li>' +
+						"<li>This project is an Influencer Reward application that allows a mall owner to register their influencers and reward them based on the product purchased by customers using the influencer's link.</li>" +
+						'<li>I learned about Laravel and ReactJS in this project.</li>',
+				},
+				{
+					date: dayjs('Feb 2020').format('MMMM YYYY'),
+					title: 'New Project',
+					details:
+						'<li>I was introduced to my 2nd project.</li>' +
+						'<li>The project is a web application that can be embedded and allows the mall owner to sync their instagram images and display it in the mall webpage.</li>' +
+						'<li>I learned about VueJS in this project</li>',
+				},
+				{
+					date: dayjs('May 2020').format('MMMM YYYY'),
+					title: 'First Official Project',
+					details:
+						'<li>I was assigned to revamp a legacy application to enhance the navigation and simplify the processes.</li>' +
+						'<li>The project is an Employee Information System which allows the employee to view their information, attendance, apply for leave application, and monitor their leave credits.</li>' +
+						'<li>This also allows the admin to monitor each employees attendance, leave applications, customize leave rules, and specify work days and holidays.</li>' +
+						'<li>In this project, I learned about docker, redis, laravel queues, and server sent events.</li>',
+				},
+				{
+					date: dayjs('May 2021').format('MMMM YYYY'),
+					until: dayjs('May 2022').format('MMMM YYYY'),
+					title: 'Exploring Web Technologies',
+					details:
+						'<li>Aside from what I used at work, I also explore other web technologies such as GraphQL, VueApollo, Laravel Lighthouse, Firebase, and TailwindCSS.</li>' +
+						'<li>I also bought a VueJS and ReactJS course to further improve my knowledge on my current stacks.</li>',
+				},
+			],
+		};
+	},
 };
 </script>
