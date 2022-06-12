96e081c55e39b4048c9dad4ee9849f69231febc3
Nad Lambino <ronaldlambino0000@gmail.com>
2022-06-12 19:06:52 +0800

    fix wip page margin top for mobile landscape

--- src/components/reusables/WorkInProgress.vue
+++ src/components/reusables/WorkInProgress.vue
@@ -5,11 +5,12 @@
 			name="io-hammer-sharp"
 			animation="wrench"
 			speed="slow"
+			class="hammer-icon"
 		></v-icon>
-		<h1 class="text-center">Work In Progress</h1>
-		<p>
+		<h1 class="wip-text">Work In Progress</h1>
+		<p class="wip-sub-text">
 			Sorry for the inconvenience
-			<v-icon scale="2" name="hi-emoji-sad"></v-icon>
+			<v-icon class="sad-icon" scale="2" name="hi-emoji-sad"></v-icon>
 		</p>
 	</div>
 </template>
@@ -22,18 +23,22 @@ export default {
 
 <style scoped lang="scss">
 .wip-container {
-	@apply flex items-center align-middle flex-col;
+	@apply h-full flex items-center align-middle flex-col;
 
-	h1,
-	svg {
-		@apply text-[70px] md:text-8xl text-gray-500 font-bold opacity-[0.8];
+	.hammer-icon {
+		@apply mb-10;
 	}
 
-	p {
+	.hammer-icon,
+	.wip-text {
+		@apply text-[70px] md:text-8xl text-gray-500 font-bold opacity-[0.8] text-center;
+	}
+
+	.wip-sub-text {
 		@apply text-4xl text-gray-500 opacity-[0.5] relative top-10 text-center;
 
-		svg {
-			@apply relative -top-2;
+		.sad-icon {
+			@apply relative -top-[10px];
 		}
 	}
 }
